464196adb43bea52caa097eb0cb22fc73522921c
shimanekb <brandon.shimanek@hotmail.com>
2015-08-08 16:30:53 -0500

    finished section how to be lazy ch4

--- chapter4/functional.clj
+++ chapter4/functional.clj
@@ -22,3 +22,17 @@
               current
               (recur next (+ current next) (dec n))))]
          (fib 0N 1N n)))
+
+(defn lazy-seq-fibo
+  ([]
+   (concat [0 1] (lazy-seq-fibo 0N 1N)))
+  ([a b]
+        (let [n (+ a b)]
+          (lazy-seq
+            (cons n (lazy-seq-fibo b n))))))
+
+(defn fibo []
+  (map first (iterate (fn [[a b]] [b (+ a b)]) [0N 1N])))
+
+;holds teh head (avoid!)
+(def head-fibo (lazy-cat [0N 1N] (map + head-fibo (rest head-fibo))))
